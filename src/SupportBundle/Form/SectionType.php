ad5989dbab8c11e30381ad757b4cb23021c5eb36
Antoine Subit <antoine.subit@gfi.fr>
2016-01-22 17:22:06 +0100

    [add] Section & Article CRUD + Collapse accordion CSS/JS

---
+++ src/SupportBundle/Form/SectionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SupportBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SectionType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'SupportBundle\Entity\Section'
+        ));
+    }
+}
