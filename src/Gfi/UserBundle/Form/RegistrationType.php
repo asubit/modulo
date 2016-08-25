47f06609854862cdc5cffcb51ded44cfce422618
Antoine Subit <antoine.subit@gfi.fr>
2016-08-25 11:36:14 +0200

    [register] Add custom field on registration + Templating register form

---
+++ src/Gfi/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Gfi\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('company')
+            ->add('website')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('phone')
+        ;
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Gfi\UserBundle\Entity\User'
+        ));
+    }
+}
