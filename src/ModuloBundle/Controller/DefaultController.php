ca3f1a7f7f1efc578e9f885470a32a025cf9e78f
Antoine Subit <antoine@subit.fr>
2017-09-21 10:06:00 +0200

    [product] Remove search product

--- src/ModuloBundle/Controller/DefaultController.php
+++ src/ModuloBundle/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace ModuloBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use ModuloBundle\Repository\ProductRepository;
 
 class DefaultController extends Controller
 {
@@ -15,14 +14,8 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $products = $em->getRepository("ModuloBundle:Product")->findAll();
 
-        return $this->render(
-            'ModuloBundle:Default:index.html.twig',
-            array(
-                'products' => $products
-            )
-        );
+        return $this->render('ModuloBundle:Default:index.html.twig');
     }
 
     /**
@@ -32,25 +25,4 @@ class DefaultController extends Controller
     {
         return $this->render('ModuloBundle:Default:admin.html.twig');
     }
-
-    /**
-     * @Route("/find-match", name="find_match")
-     * @Method({"GET", "POST"})
-     */
-    public function findMatchAction()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $zipCode = $this->get('request')->request->get('zip_code');
-        $productId = $this->get('request')->request->get('product_id');
-        $matchs = $em->getRepository("ModuloBundle:Product")->findClose($zipCode, $productId);
-        return $this->render(
-            'ModuloBundle:Default:find-match.html.twig',
-            array(
-                'matchs' => $matchs
-            )
-        );
-    }
-
-
 }
