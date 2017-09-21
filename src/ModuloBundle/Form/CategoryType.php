9ac641089d254afc5dea01e9463ebb3e99af56ca
Antoine Subit <antoine@subit.fr>
2017-09-21 15:01:52 +0200

    [cms] Create Category CRUD

---
+++ src/ModuloBundle/Form/CategoryType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ModuloBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ModuloBundle\Entity\Category'
+        ));
+    }
+}
